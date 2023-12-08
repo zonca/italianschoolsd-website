864d329be20a111b083a357c00f49cca3602d990
Andrea Zonca <code@andreazonca.com>
2023-12-07 20:39:03 -0800

    hotlink protection for images and immutable

---
+++ netlify/edge-functions/hotlink-protection.ts
@@ -0,0 +1,18 @@
+import type { Context } from "<https://edge.netlify.com>";
+
+export default async (
+  request: Request,
+  context: Context
+): Promise<Response> => {
+
+  const referer = request.headers.get("referer");
+    const prodRegex = /^https?:\/\/(.*\.)?italianschoolsd\.com(\/.*)?$/;
+const devRegex = /^https?:\/\/(.*\--)?italianschoolsd-com\.netlify\.app(\/.*)?$/;
+
+if (
+  !referer ||
+  !(prodRegex.test(referer) || devRegex.test(referer))
+) {
+  return new Response("Forbidden", { status: 403 });
+}
+};
