76fbede1f4f49d1c6e75f06eb6c2b2f409678f30
Andrea Zonca <code@andreazonca.com>
2025-07-19 20:02:59 +0200

    refactor(algolia-indexer): replace atomic-algolia with official client for indexing

--- algolia-indexer.js
+++ algolia-indexer.js
@@ -1,21 +1,23 @@
 const algoliasearch = require("algoliasearch");
-const atomic = require("atomic-algolia");
+const fs = require("fs");
 
 const client = algoliasearch(
   process.env.ALGOLIA_APP_ID,
   process.env.ALGOLIA_ADMIN_KEY
 );
+const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
 
-atomic(client, process.env.ALGOLIA_INDEX_NAME, {
-  files: "./dist/search.json",
-  transform: ({ content, ...rest }) => ({
-    ...rest,
-    content: content.substring(0, 5000),
-  }),
-}, (err, result) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-    console.log("Finished indexing", result);
-});
+const records = JSON.parse(fs.readFileSync("./dist/search.json", "utf8"));
+
+// atomic-algolia seems to be buggy, let's use the official client
+index
+  .replaceAllObjects(records, {
+    autoGenerateObjectIDIfNotExist: true,
+  })
+  .then(({ objectIDs }) => {
+    console.log(`Finished indexing ${objectIDs.length} records.`);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
